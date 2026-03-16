d05c028fc1f2b3c97320b4a1f92d2777cc0c583e
Tiza <tizianoagustinrojas@gmail.com>
2026-03-15 22:46:12 -0300

    updated models

--- app/models.py
+++ app/models.py
@@ -1,13 +1,35 @@
 from app.extensions import db
+from datetime import datetime
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    posts = db.relationship("Post")
+    created_at = db.Column(db.DateTime)
+    comments = db.relationship("Comment", backref="user", lazy=True)
+    posts = db.relationship("Post", backref="user", lazy=True)
+    
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    created_at = db.Column(db.DateTime) 
+    likes = db.relationship("Like", backref="comment", lazy=True)
+
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comment_id = db.Column(db.Integer, db.ForeignKey('comment.id'), nullable=False)
+    created_at = db.Column(db.DateTime)
+    __table_args__ = (db.UniqueConstraint('user_id', 'comment_id'))
